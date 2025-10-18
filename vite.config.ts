aafe6ae360b0ee8d6f8aa1da8bf29c3f8977551b
Alex Krol <krolmail@list.ru>
2025-10-19 00:42:51 +0300

    add types generation, remove unnecessary file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    })
+  ],
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
   },
